6e704835a08bf81bdd2c0b0a8fd8e8b429e86045
raidyue <yxd506388664@163.com>
2015-12-02 00:04:27 +0800

    manager...

--- app/manager/views.py
+++ app/manager/views.py
@@ -1,6 +1,6 @@
 # encoding=utf-8
 __author__ = 'raidyue'
-from flask import Blueprint,render_template,session
+from flask import Blueprint, render_template, session, request
 from ..models import *
 
 manager_bp = Blueprint('manager', __name__, url_prefix='/manager', template_folder='templates')
@@ -9,6 +9,14 @@ manager_bp = Blueprint('manager', __name__, url_prefix='/manager', template_fold
 @manager_bp.route('/')
 def index():
     if 'manager' in session:
-        pass
+        return 'login manager'
 
     return render_template('htllo.html')
+
+
+@manager_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET':
+        return render_template('manager_login.html')
